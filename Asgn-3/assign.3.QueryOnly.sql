037d2a08a4d108767a4734fbc786733a0f2f5088
oldcoyote03 <nickolas@Nickolass-MacBook-Pro.local>
2013-10-10 00:21:53 -0700

    asgn 3

---
+++ Asgn-3/assign.3.QueryOnly.sql
@@ -0,0 +1,86 @@
+#1.a
+SELECT model, speed, hd FROM pc WHERE price > 1000
+#1.b
+SELECT model, speed AS gigahertz, hd AS gigabytes FROM pc WHERE price > 1000
+#1.c
+SELECT DISTINCT(maker) FROM product WHERE ctype = 'printer'
+#1.d
+SELECT model, ram, screen FROM laptop WHERE price > 1000
+#1.e
+SELECT * FROM printer WHERE color
+#1.f
+SELECT model, hd FROM pc WHERE speed = 3.2 AND price < 2000
+#2.a
+SELECT maker, speed FROM laptop NATURAL JOIN product WHERE hd >= 30
+#2.b
+SELECT model, price FROM pc NATURAL JOIN product WHERE maker = 'B'
+UNION
+SELECT model, price FROM laptop NATURAL JOIN product WHERE maker = 'B'
+UNION
+SELECT model, price FROM printer NATURAL JOIN product WHERE maker = 'B'
+#2.c
+SELECT DISTINCT(maker) FROM product WHERE ctype = 'laptop' AND maker NOT IN
+(SELECT DISTINCT(maker) FROM product WHERE ctype = 'pc')
+#2.d
+SELECT DISTINCT(a.hd) FROM pc a JOIN pc b 
+WHERE a.hd = b.hd AND a.model > b.model
+#2.e
+SELECT a.model as First, b.model as Second FROM pc a JOIN pc b
+WHERE a.speed = b.speed AND a.ram = b.ram AND a.model > b.model
+#2.f
+SELECT a.maker
+FROM ( SELECT maker, speed FROM pc NATURAL JOIN product
+WHERE speed >= 2.0
+UNION SELECT maker, speed FROM laptop NATURAL JOIN product
+WHERE speed >= 2.0 ) a
+GROUP BY a.maker
+HAVING count(a.maker) > 1
+#3.a
+SELECT maker FROM product WHERE 3.0 <= ALL (
+SELECT speed FROM pc)
+#3.b
+SELECT model FROM printer exp WHERE price > ALL (
+SELECT price FROM printer WHERE NOT model = exp.model)
+#3.c
+SELECT model FROM laptop WHERE speed < ALL (
+SELECT speed FROM pc)
+#3.d
+SELECT model FROM (SELECT model, price FROM pc
+UNION SELECT model, price FROM laptop UNION
+SELECT model, price FROM printer) a
+WHERE a.price >= ALL (SELECT price FROM pc
+UNION SELECT price FROM laptop UNION SELECT price FROM printer)
+#3.e
+SELECT maker FROM product a JOIN printer b ON a.model = b.model
+WHERE b.price <= ALL (SELECT price FROM printer)
+#3.f
+SELECT a.maker FROM (SELECT maker, speed
+FROM pc NATURAL JOIN product WHERE ram <= ALL (SELECT ram FROM pc) ) a
+WHERE a.speed >= ALL (SELECT speed FROM pc)
+#4.a
+SELECT AVG(speed) FROM pc
+#4.b
+SELECT AVG(speed) FROM laptop WHERE price > 1000
+#4.c
+SELECT AVG(price) FROM pc NATURAL JOIN product WHERE maker = 'A'
+#4.d
+SELECT AVG(a.price) FROM (
+SELECT price FROM pc NATURAL JOIN product WHERE maker = 'B'
+UNION
+SELECT price FROM laptop NATURAL JOIN product WHERE maker = 'B' ) a
+#4.e
+SELECT speed, AVG(price) FROM pc GROUP BY speed
+#4.f
+SELECT b.maker, AVG(a.screen) FROM laptop a NATURAL JOIN product b
+GROUP BY b.maker
+#4.g
+SELECT maker FROM product NATURAL JOIN pc
+GROUP BY maker HAVING count(*) >= 3
+#4.h
+SELECT maker, MAX(price) FROM product NATURAL JOIN pc GROUP BY maker
+#4.i
+SELECT speed, AVG(price) FROM pc GROUP BY speed HAVING speed > 2.0
+#4.j
+SELECT maker, AVG(hd) FROM pc NATURAL JOIN product
+WHERE maker IN (SELECT maker FROM product WHERE ctype = 'printer')
+GROUP BY maker
---
+++ Asgn-3/assign.3.py
@@ -0,0 +1,36 @@
+
+import MySQLdb
+
+def QueryOutputs():
+   loadPre = "SELECT * FROM ("
+   loadMid = ") qali INTO OUTFILE '/tmp/assign.3."
+   loadSuf1 = ".csv' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' "
+   loadSuf2 = "LINES TERMINATED BY '\\n'"
+
+   conn = MySQLdb.connect (host = "localhost",
+                           user = "nvela",
+                           passwd = "nuance",
+                           db = "computers")
+   cursor = conn.cursor ()
+
+   # Get queries and file names from .sql file and output to .csv files
+   myDir = raw_input("Enter the absolute path of the .sql file\n")
+   fin = open(myDir,'r')
+   fileSuf = fin.readline()[1:].strip('\n'); qLine = ''
+   curr = fin.readline()
+   while curr:
+      if curr[0] == '#':
+         cursor.execute(loadPre+qLine[:len(qLine)-1]+loadMid+fileSuf+loadSuf1+loadSuf2)
+         fileSuf = curr[1:].strip('\n'); qLine = ''
+         curr = fin.readline()
+      else:
+         while curr and (curr[0] != '#'):
+            qLine += curr.strip('\n') + ' '
+            curr = fin.readline()
+   cursor.execute(loadPre+qLine[:len(qLine)-1]+loadMid+fileSuf+loadSuf1+loadSuf2)
+   print '\n\n Query outputs are in /tmp directory.'
+ 
+   cursor.close ()
+   conn.close ()
+
+   fin.close()
